1ebddd7d7fed46cc2f6e38ca4e5eaec085818d45
gustavoabell <gustavoabel.contato@gmail.com>
2021-06-25 09:46:44 -0300

    connection db and exceptions

--- src/database/migrations/1624359084734-CreateUsers.ts
+++ src/database/migrations/1624359084734-CreateUsers.ts
@@ -1,44 +1,43 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1624359084734 implements MigrationInterface {
+export class CreateUsers1624320908885 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
                 name: "users",
-                columns:[
+                columns: [
                     {
                         name: "id",
                         type: "uuid",
-                        isPrimary: true,
-                    },
+                        isPrimary: true
+                    }, 
                     {
                         name: "name",
-                        type: "varchar" 
-                    },
+                        type: "varchar"
+                    }, 
                     {
                         name: "email",
                         type: "varchar"
-                    },
+                    }, 
                     {
                         name: "admin",
                         type: "boolean",
                         default: false
-                    },
+                    }, 
                     {
                         name: "created_at",
                         type: "timestamp",
                         default: "now()"
-                    },
+                    },       
                     {
-                        name:"updated_at",
+                        name: "updated_at",
                         type: "timestamp",
                         default: "now()"
-                    },
-
-                ]
+                    },         
+                ],
             })
-        )
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,32 @@
-import {Entity} from "typeorm";
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
 
-@Entity()
+@Entity("users") //referencia a tabela users do BD
 class User {
+
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
 
 }
 
